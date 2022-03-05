03dfaf35f3a5509a84497d41a06b85d4ba4615dd
mdarius13 <mdarius13@icloud.com>
2022-03-05 21:06:16 +0100

    Implement Custom Idle Callbacks requests

---
+++ Source/FiberDOM/RequestIdleCallback.ts
@@ -0,0 +1,65 @@
+//
+//  FiberDOM/RequestIdleCallback.ts
+//  Viridian FiberDOM Idle Callbacks.
+//
+//  Created by Darius Miclaus (mdarius13)
+//
+//  Copyright (c) 2022 Miclaus Industries Corporation B.V. Advanced Software Technologies Research Group.
+//
+//  Permission is hereby granted, free of charge, 
+//  to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+//  to deal in the Software without restriction, including without limitation the rights to use, copy, 
+//  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+//  persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall 
+//  be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+//  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+//  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+//  OR OTHER DEALINGS IN THE SOFTWARE.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+//
+
+
+/**
+ * Request Idle Callback Handle type
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type RequestIdleCallbackHandle = any;
+
+/**
+ * Request Idle Callback Options interface
+ */
+interface RequestIdleCallbackOptions {
+  timeout: number;
+}
+
+
+/**
+ * Request Idle Callback Deadline interface
+ */
+interface RequestIdleCallbackDeadline {
+    /** Did timeout*/
+    readonly didTimeout: boolean;
+
+    /** Time Remaining */
+    timeRemaining: (() => number);
+}
+
+/** Global scope */
+declare global {
+    /** Global Window */
+	interface Window {
+        /** Request idle callback */
+		requestIdleCallback: ((callback: ((deadline: RequestIdleCallbackDeadline) => void), opts?: RequestIdleCallbackOptions,) => RequestIdleCallbackHandle);
+		
+        /* Cancel idle callback */
+        cancelIdleCallback: ((handle: RequestIdleCallbackHandle) => void);
+	}
+  }
