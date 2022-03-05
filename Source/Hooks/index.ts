3cd12ec27a9e3419782be3ff8e4972a7cfa5336c
mdarius13 <mdarius13@icloud.com>
2022-03-05 19:05:24 +0100

    Implement Hooks index file
    Add VRHooks export file

---
+++ Source/Hooks/index.ts
@@ -0,0 +1,46 @@
+//
+//  Hooks/index.ts
+//  Index file for the Viridian FiberDOM Hooks
+//
+//  Created by Darius Miclaus (mdarius13)
+//
+//  Copyright (c) 2022 Miclaus Industries Corporation B.V. Advanced Software Technologies Research Group.
+//
+//  Permission is hereby granted, free of charge, 
+//  to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+//  to deal in the Software without restriction, including without limitation the rights to use, copy, 
+//  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+//  persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall 
+//  be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+//  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+//  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+//  OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+/* Include Directive */
+import { $reference } from "./$reference"; // $reference Hook
+import { $state } from "./$state";         // $state     Hook
+
+/**
+ * Viridian FiberDOM Hooks
+ */
+export const VRHooks = {
+	/** Hook that allows you to persist values between renders. 
+	* 
+	* It can be used to store a mutable value that does not cause a re-render when updated. 
+	* It can be used to access a DOM element directly. */
+	$reference,
+
+	/** Hook that allows you to have state variables in functional components.
+
+	You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state),
+	and another function to update this value. */
+	$state,
+};
